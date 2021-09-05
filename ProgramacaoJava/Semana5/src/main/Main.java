661c7f5ac27db67c57b88e7f1238b267624c27df
Gabriel Franca <gabriel.franca@gmail.com>
2021-09-05 19:08:21 -0300

    Add files via upload

---
+++ ProgramacaoJava/Semana5/src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import dominio.DiaDaSemana;
+
+public class Main {
+
+	public static void main(String[] args) {
+		var lista = new ArrayList<String>(Arrays.asList(args));
+		
+		{lista.stream()
+			.filter(Main::numInt)
+			.map(Integer::valueOf)
+			.distinct()
+			.filter(d -> d <= 0 || d >= 8)
+			.forEach(d -> System.out.println(d));
+			}
+	
+		lista.stream()
+			  .filter(Main::numInt)
+		      .map(Integer::valueOf)
+		      .distinct()
+		      .filter(d -> d > 0 && d < 8)
+		      .map(DiaDaSemana::new)
+		      .forEach(d -> System.out.println(d.toString()));
+
+	}
+
+	public static boolean numInt(String text) {
+		try {
+			Integer.valueOf(text);
+			return true;
+		} catch (NumberFormatException e) {
+			System.out.println(text);
+			return false;
+			
+		}
+			
+	}
+}
